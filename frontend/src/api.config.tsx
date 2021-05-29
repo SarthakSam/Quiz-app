10b8e650092404b77d1ec800d58a8cc7978f9b20
Sarthak <sarthak242@gmail.com>
2021-05-29 13:12:09 +0530

    updated baseURL

--- frontend/src/api.config.tsx
+++ frontend/src/api.config.tsx
@@ -12,7 +12,7 @@ type urlKey = 'specificQuizes' | 'categories' | 'postQuiz' | 'signup' | 'signin'
 const baseUrl = 'https://test-ur-knowledge.herokuapp.com';
 
 export function getUrl( url: urlKey, params: any = {} ) {
-    return Object.keys(params).reduce((acc: string, cur: string) => {
+    return baseUrl + Object.keys(params).reduce((acc: string, cur: string) => {
         return acc.replace(`:${cur}`, params[cur]);
     }, urlMapping[url]);
 }
